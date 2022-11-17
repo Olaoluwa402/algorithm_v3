3e2a037a2505aa8e5739a0da1a2bce12f0839225
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2022-11-17 09:14:12 +0100

    algorithm challege - 3

--- js/main.js
+++ js/main.js
@@ -54,3 +54,13 @@ function evenInt(nums) {
         4. Write a function that returns true if a number passed into it is an 
         even number and false if otherwise. 
     */
+
+/* 
+        5. Create a function that takes a number as an argument, increments the number by 
+        1 and return the result. Example addition(0) => 1, addition(4) => 5
+    */
+
+/* 
+       6. Create a function that takes voltage and current and returns the 
+        calculated power. Example circuitPower(230, 10) => 2300
+    */
