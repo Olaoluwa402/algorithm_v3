cbaa3ffa91a3a7aa33f1ad69b1d16bb0073a9830
unknown <supertlux@gmail.com>
2022-11-21 02:13:11 -0800

    starter

--- DaraDjango/dara.js
+++ DaraDjango/dara.js
@@ -18,15 +18,15 @@
         4. Write a function that returns true if a number passed into it is an 
         even number and false if otherwise. 
     */
-        function check(x){
-            if(x%2==0){
-                return true;
-            }
-            else{
-               return false;
-            }
-        }
-            console.log(check(60))
+        // function check(x){
+        //     if(x%2==0){
+        //         return true;
+        //     }
+        //     else{
+        //        return false;
+        //     }
+        // }
+        //     console.log(check(60))
             
     
 
@@ -38,25 +38,73 @@
         5. Create a function that takes a number as an argument, increments the number by 
         1 and return the result. Example addition(0) => 1, addition(4) => 5
     */
-   let count;
-   function num(x){
+//    let count;
+//    function num(x){
     
-    count= x+1 ;
-    return count;
+//     count= x+1 ;
+//     return count;
 
-   }
-   console.log(num(5));
+//    }
+//    console.log(num(5));
 
 /* 
        6. Create a function that takes voltage and current and returns the 
         calculated power. Example circuitPower(230, 10) => 2300
     */
 
-        function pow(x,y){
-            z=x*y
-            return(z);
-            }
+        // function pow(x,y){
+        //     z=x*y
+        //     return(z);
+        //     }
             
 
         
-        console.log(pow(20,30));
+        // console.log(pow(20,30));
+
+
+
+        /*
+       7. You are counting points for a basketball game, given the amount of 
+        2-pointers scored and 3-pointers scored, find the final points for the 
+        team and return that value. Examples points(1,1) => 5, points(5,7) => 31
+    */
+
+        // function point(x,y){
+        //     let twoPointers=2*x;
+        //     let threePointers=3*y;
+        //     let teamTotal=twoPointers + threePointers
+            
+        //     return(teamTotal)
+        // }
+        // console.log(point(6,8));
+
+
+        // function Points(x,y){
+        //     let p=(2*x) + (3*y)
+        //     return(p)  
+        // }
+        // console.log(Points(5,8))
+/*
+    8.  Repeat a given string str (first argument) for num times (second argument). 
+      Return an empty string if num is not a positive number. 
+      For the purpose of this challenge, do not use the built-in .repeat() method.
+
+      Example:
+      repeatStringNumTimes("*", 3) should return the string ***.
+       repeatStringNumTimes("abc", 3) should return the string abcabcabc.
+  */
+    //     let stng=("");
+    //    function stringRepeat(x,num){
+    //     if(num<0){
+    //         repeat=stng;
+    //     }
+    //     else{
+    //         repeat=x*num
+    //     }
+    //     return(repeat);
+
+    //    }
+    //    console.log(stringRepeat("dara",4));
+
+let y="dara" * 4;
+console.log (y);
