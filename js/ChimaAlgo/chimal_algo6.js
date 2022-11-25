ec8ee3c9d5aac389de0309e5975eafe8720ef1ef
Chima <ugoppromise@gmail.com>
2022-11-25 13:09:39 +0100

    my wrong solution

--- js/ChimaAlgo/chimal_algo6.js
+++ js/ChimaAlgo/chimal_algo6.js
@@ -7,13 +7,41 @@
 
         Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 
-        sumPrimes(10) should return 17.
+        sumPrimes(15) should return 17.
         sumPrimes(977) should return 73156.
 
         note:We only need to check up to the square root because the square root 
         of a number is the largest possible unique divisor.
     */
 let sumPrimes = (x)=>{
+    let primeArray =[];
+    let secondPrimeArray=[];
+    let thirdPrimeArray=[];
+    for(let i=2;i<=x; i++){
+    primeArray.push(i)
+    }
+    for(let i = 0; i<primeArray.length; i++){
+        let k = 2;
+        if(primeArray[i] % k != 0){
+            secondPrimeArray.push(primeArray[i])
+        }
+    }
+    for(let i = 0; i<secondPrimeArray.length; i++){
+        let newv = Math.sqrt(secondPrimeArray[i] )
+        let newv2 = secondPrimeArray[i+5] % 5 
+        if( /[\.]/.test(String(newv)) === true || newv2 == 0)
+        {
+            thirdPrimeArray.push(secondPrimeArray[i])
+        }
+    }
+    thirdPrimeArray.unshift(2)
+    let finalValue = 0
+    for(let i=0; i < thirdPrimeArray.length; i++){
+        finalValue += thirdPrimeArray[i]
+    }
 
-
+console.log(finalValue)
 }
+//wrong wrong wrong
+// console.log(sumPrimes(10))
+sumPrimes(977)
