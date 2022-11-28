ff8f8d7c0d053fb1a8a4d7742e45609be43f5aac
unknown <supertlux@gmail.com>
2022-11-28 00:36:26 -0800

    dara

--- js/DaraDjango/dara.js
+++ js/DaraDjango/dara.js
@@ -70,14 +70,14 @@
     */
         // <---I DID THIS MYSELF ----->
 
-        function point(x,y){
-            let twoPointers=2*x;
-            let threePointers=3*y;
-            let teamTotal=twoPointers + threePointers
+        // function point(x,y){
+        //     let twoPointers=2*x;
+        //     let threePointers=3*y;
+        //     let teamTotal=twoPointers + threePointers
             
-            return(teamTotal)
-        }
-        console.log(point(6,8));
+        //     return(teamTotal)
+        // }
+        // console.log(point(6,8));
 
 
         // function Points(x,y){
@@ -113,15 +113,66 @@
 
     // <<------THIS IS S CLASS CORRRECTION------>
 
-let stringRepeat=(str,num)=>{
-    let new_str=("");
-    if (num >0){
-        for (let i=0; i<num; i++){
-            new_str=new_str +str;
-        }
-        return new_str;
-    }else {
-        return("")
-    }
-}
-console.log(stringRepeat("abc", 6));
+// let stringRepeat=(str,num)=>{
+//     let new_str=("");
+//     if (num >0){
+//         for (let i=0; i<num; i++){
+//             new_str=new_str +str;
+//         }
+//         return new_str;
+//     }else {
+//         return("")
+//     }
+// }
+// console.log(stringRepeat("abc", 6));
+
+
+
+
+/* 10
+        SUM ALL PRIME:
+        A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2
+        is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is
+        divisible by 1, 2 and 4.
+
+        Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+        sumPrimes(10) should return 17.
+        sumPrimes(977) should return 73156.
+
+        note:We only need to check up to the square root because the square root 
+        of a number is the largest possible unique divisor.
+    */
+
+        
+       function sumPrimes(x){
+        let sum=0;
+        // we loop through from 2 num
+        function isPrime(x){
+            for (let num=2; num<=x-1; num++){
+                return (num);
+
+            // if (x>1 && x%num!==0){
+            //     return x;
+            }
+        
+            }
+            isPrime(5);
+
+
+ 
+
+
+       }
+       console.log(sumPrimes(40));
+
+    } 
+
+        
+
+
+        
+        sumPrimes(7);
+        
+
+        
