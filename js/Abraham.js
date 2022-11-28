d97cee2e565ccd7b9f87a556890a7fa0f1699106
aridunnu1 <abraham4top@gmail.com>
2022-11-28 09:38:50 +0100

    Question

--- js/Abraham.js
+++ js/Abraham.js
@@ -209,3 +209,61 @@ function sumAll(arr) {
   }
   console.log(sumAll([10, 5]));
   
+  /* 10
+        SUM ALL PRIME:
+        A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2
+        is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is
+        divisible by 1, 2 and 4.
+
+        Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+        sumPrimes(10) should return 17.
+        sumPrimes(977) should return 73156.
+
+        note:We only need to check up to the square root because the square root 
+        of a number is the largest possible unique divisor.
+    */
+
+        function sumprimes(num){
+          let sum = 0
+          function isprime(num){
+            for(let i=2; i<num; i++){
+              if(num % i == 0){
+                return false
+              }
+            } 
+            return true
+          }
+            for(let i=2; i<=num; i++){
+              if (isprime(i)){
+                sum += i
+              }
+            } 
+            return sum
+        }
+        console.log(sumprimes(10))
+        console.log(sumprimes(977))
+
+  // solution 2
+         function sumPrime(num){
+          // 1. loop through the num given, excluding zero and 1
+          // 2. initiate a sum count
+          let sum = 0
+          for(let i=2; i<=num; i++){
+            if(isPrime(i)){
+              sum =sum +i;
+            }
+          }
+           return sum;
+        
+         function isPrime(num){
+          // check that num is prime 
+          for(let i=2; i<num; i++){
+            if(num % i === 0){
+              return false;
+            }
+          }
+          return true;
+         }
+        }
+        console.log(sumPrime(12))
