66c834bb7ed5bc13b024698f8d79a4d9728048e2
dedayorr <dedayo16@gmail.com>
2022-11-28 12:54:13 +0100

    commited

--- js/dayo.js
+++ js/dayo.js
@@ -231,3 +231,39 @@ NOTE; Return the value of the result from the function*/
           }
         }
 
+      //  11.
+      // A tetrahedron is a pyramid with a triangular base and three sides. 
+      // A tetrahedral number is a number of items within a tetrahedron.
+      // Create a function that takes an integer n and returns the nth tetrahedral number.
+      
+      // Hint :
+      // tetra(2) ➞ 4
+      // tetra(5) ➞ 35
+      // tetra(6) ➞ 56
+      
+      function tetra(n){
+        return (n*(n+1)*(n+2))/6
+      }
+      console.log(tetra(2))
+      console.log(tetra(5))
+
+
+
+
+//     12. Write a function redundant that takes in a string str and returns a function that returns str.
+//       const f1 = redundant("apple")
+//       f1() ➞ "apple"
+//       const f2 = redundant("pear")
+//       f2() ➞ "pear"
+//       const f3 = redundant("")
+//       f3() ➞ ""
+//      
+        function redundant(str){
+          return function f1(){
+            return str;
+          }
+
+        }
+        const result = redundant("folake");
+        console.log(redundant("dayo"))
+
