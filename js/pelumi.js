e7c4b9bb0904839ffba502a19dc65bb2efbaab63
Oluwapelumi <pelumiiadetoye@gmail.com>
2022-12-05 11:49:20 +0100

    worked on algo 15 and 16

--- js/pelumi.js
+++ js/pelumi.js
@@ -308,7 +308,84 @@ Create a function that takes a number x and a character y ("m" for male, "f" for
 generation(2, "f") ➞ "granddaughter"
 generation(-3, "m") ➞ "great grandfather"
 generation(1, "f") ➞ "daughter"
- */
-function generation(x, y){
-     
-}
+
+let generation = (gen, gender) => {
+  let g = {
+    "3": ["great grandson", "great granddaughter"],
+    "2": ["grandson", "granddaughter"],
+    "1": ["son", "daughter"],
+    "0": ["me", "me"],
+   "-1": ["father", "mother"],
+   "-2": ["grandfather", "grandmother"],
+   "-3": ["great grandfather", "great grandmother"]
+  };
+  
+      return gender === "m" && gen > -4 && gen < 4
+    ? g[gen][0]
+    : gender === "f" && gen > -4 && gen < 4
+    ? g[gen][1]
+    : "out of scope"; 
+};
+console.log(generation(0, "m"));
+
+*/
+
+/*
+15.
+    Integer Reversal
+    Given an integer, reverse the order of the digits
+
+    reverseInteger(123) => 321
+    reverseInteger(46923) => 32964
+*/
+
+    // function reverseInteger(number){
+    //     number += '';
+    //     return number.split('').reverse().join('');
+        
+    // }
+    // console.log(reverseInteger(123));
+
+/*
+16.
+        Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+        First argument is the sentence to perform the search and replace on.
+
+        Second argument is the word that you will be replacing (before).
+
+        Third argument is what you will be replacing the second argument with (after).
+
+        Note: Preserve the case of the first character in the original word when you are replacing it. 
+        For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+
+        Template:
+        myReplace("Let us go to the store", "store", "mall") should return the string Let us go to the mall.
+
+        myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return the string He is Sitting on the couch.
+
+        myReplace("I think we should look up there", "up", "Down") should return the string I think we should look down there.
+
+*/
+// function myReplace(sentence, before, after){
+//         let newString = sentence.replace(before, after);
+//         return newString
+// }
+// console.log(myReplace("I think we should look up there", "up", "Down"));
+
+
+// function myReplace(str, before, after) {
+//   //search for the before string and then use a function to check for capitalisation before replacing.
+//   str = str.replace(before, function(x){
+//     //check whether the first letter of before is a capital
+//     //replace after with a capitalised first letter and then 
+//     //the rest of the string, cannot just replace first letter as its read only.
+//     if(before[0] == before[0].toUpperCase()){
+//       after = after[0].toUpperCase() + after.slice(1);
+//       return after;
+//     }
+//     return after;
+//   });
+//   return str;
+// }
+// console.log(myReplace("I think we should look up there", "up", "Down"));
